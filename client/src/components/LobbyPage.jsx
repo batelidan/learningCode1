04cf2772ca25a307190a20b4b66a6c942937721b
batelidan <batelidan7@gmail.com>
2023-08-27 08:14:16 +0300

    change url at get fun

--- client/src/components/LobbyPage.jsx
+++ client/src/components/LobbyPage.jsx
@@ -9,7 +9,7 @@ function LobbyPage() {
     const [items,setItems]=useState([]);
    
     useEffect(() => {
-      fetch("http://localhost:3001/lessons", {
+      fetch("https://sleepy-fortress-74328-1833960bec9d.herokuapp.com/", {
         method: "GET",
       })
         .then((res) => res.json())
